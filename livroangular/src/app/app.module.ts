fe07511de40608520e5b244ef55b2ab49b057881
BetoCruz <humbertocruz.sp@gmail.com>
2020-12-10 14:06:00 -0300

    Criação do componente interpolation , configuração de sua classe e inplementação no appComponent

--- livroangular/src/app/app.module.ts
+++ livroangular/src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 import { AppComponent } from './app.component';
 import { IterpolationBindingComponent } from './iterpolation-binding/iterpolation-binding.component';
 import { PropertyBindingComponent } from './property-binding/property-binding.component';
+import { TwoWayDataBindingComponent } from './two-way-data-binding/two-way-data-binding.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     IterpolationBindingComponent,
-    PropertyBindingComponent
+    PropertyBindingComponent,
+    TwoWayDataBindingComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
