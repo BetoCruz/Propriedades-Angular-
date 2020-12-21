caac28c4c4d5142cfed402f489effcf8c908bb68
BetoCruz <humbertocruz.sp@gmail.com>
2020-12-21 12:51:11 -0300

    Aplicação, componente base.

--- livroangular/src/app/app.module.ts
+++ livroangular/src/app/app.module.ts
@@ -6,13 +6,27 @@ import { AppComponent } from './app.component';
 import { IterpolationBindingComponent } from './iterpolation-binding/iterpolation-binding.component';
 import { PropertyBindingComponent } from './property-binding/property-binding.component';
 import { TwoWayDataBindingComponent } from './two-way-data-binding/two-way-data-binding.component';
+import { NgIfComponent } from './ng-if/ng-if.component';
+import { NgSwitchCaseComponent } from './ng-switch-case/ng-switch-case.component';
+import { NgForComponent } from './ng-for/ng-for.component';
+import { NgClassComponent } from './ng-class/ng-class.component';
+import { NgStyleComponent } from './ng-style/ng-style.component';
+import { NgContentComponent } from './ng-content/ng-content.component';
+import { EventBindingComponent } from './event-binding/event-binding.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     IterpolationBindingComponent,
     PropertyBindingComponent,
-    TwoWayDataBindingComponent
+    TwoWayDataBindingComponent,
+    NgIfComponent,
+    NgSwitchCaseComponent,
+    NgForComponent,
+    NgClassComponent,
+    NgStyleComponent,
+    NgContentComponent,
+    EventBindingComponent
   ],
   imports: [
     BrowserModule,
